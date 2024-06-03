ef11d12a91d976d2c1866ff1562f1986018ce5e9
Andrei <aformusatii@gmail.com>
2024-06-03 23:21:11 +0300

    Fixes

--- client/src/ConfigurationRepository.js
+++ client/src/ConfigurationRepository.js
@@ -3,12 +3,12 @@ import {resolveApiUrl} from "./Utils";
 class ConfigurationRepository {
 
     fetch() {
-        return fetch(resolveApiUrl('/api/configuration'))
+        return fetch(resolveApiUrl('api/configuration'))
             .then(response => response.text());
     }
 
     save(configuration) {
-        return fetch(resolveApiUrl('/api/configuration'), {
+        return fetch(resolveApiUrl('api/configuration'), {
             method: 'PUT',
             headers: {
                 'Content-Type': 'text/plain'
